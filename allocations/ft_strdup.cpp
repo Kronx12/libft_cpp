3eb32d8024bc60b06a780052bd0b3f7fe1a6dd25
Kronx12 <kronx12pro@gmail.com>
2021-02-13 11:09:09 +0100

    Patch file

--- allocations/ft_strdup.cpp
+++ allocations/ft_strdup.cpp
@@ -6,13 +6,13 @@
 /*   By: gbaud <gbaud@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 21:48:48 by gbaud             #+#    #+#             */
-/*   Updated: 2021/02/08 09:46:34 by gbaud            ###   ########lyon.fr   */
+/*   Updated: 2021/02/13 11:08:57 by gbaud            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.hpp"
 
-char	*ft_strdup(const char *s)
+char	*ft_strdup(char *s)
 {
 	char		*str;
 	size_t		i;
