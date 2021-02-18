ecc223124aa877fe9cef41c64fac6ea9ad2b9d05
Kronx12 <kronx12pro@gmail.com>
2021-02-18 12:27:40 +0100

    Add conf

--- converters/ft_atoi_base.cpp
+++ converters/ft_atoi_base.cpp
@@ -1,80 +1,70 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi_base.cpp                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gbaud <gbaud@student.42lyon.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/12 16:38:50 by gbaud             #+#    #+#             */
-/*   Updated: 2021/02/13 11:13:47 by gbaud            ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
-
-
 #include "../libft.hpp"
 
-int		ft_check_base(char *base)
+int        ft_check_base(char *base)
 {
-	int i;
-	int j;
-	char compare[] = {'+', '-', ' ', '\r', '\f', '\v', '\n', '\t'};
-	
-	i = 0;
-	while(base[i])
-	{
-		j = 0;
-		while(compare[j])
-		{
-			if (base[i] == compare[j])
-				return (0);
-			j++;
-		}
-		j = 0;
-		while(base[j])
-		{
-			if (base[i] == base[j] && i != j)
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	return (i);
+    int i;
+    int j;
+    char compare[] = {'+', '-', ' ', '\r', '\f', '\v', '\n', '\t'};
+
+    i = 0;
+    while(base[i])
+    {
+        j = 0;
+        while(compare[j])
+        {
+            if (base[i] == compare[j])
+                return (0);
+            j++;
+        }
+        j = 0;
+        while(base[j])
+        {
+            if (base[i] == base[j] && i != j)
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (i);
 }
 
-int		ft_get_value(char *base, char c)
+int        ft_get_value(char *base, char c)
 {
-	int i;
+    int i;
 
-	i = 0;
-	while (base[i])
-	{
-		if (base[i] == c)
-			return (i);
-		i++;
-	}
-	return (0);
+    i = 0;
+    while (base[i])
+    {
+        if (base[i] == c)
+            return (i);
+        i++;
+    }
+    return (-1);
 }
 
-int		ft_atoi_base(char *str, char *base)
+int        ft_atoi_base(char *str, char *base)
 {
-	int i;
-	int base_len;
-	int val;
-	int val_len;
+    int i;
+    int base_len;
+    int val;
+    int val_len;
 
-	i = 0;
-	val = 0;
-	val_len = ft_strlen(str);
-	base_len = ft_check_base(base);	
-	if (base_len > 0)
-	{
-		while (str[i])
-		{
-			val += (base_len * ft_get_value(base, str[i]));
-			i++;
-		}
-		return (val);
-	}
-	else
-		return (0);
+    i = 0;
+    val = 0;
+    val_len = ft_strlen(str);
+    base_len = ft_check_base(base);
+    if (base_len > 0)
+    {
+        while (str[i])
+        {
+            if (ft_get_value(base, str[i]) == -1)
+                return(val);
+            val *= base_len;
+            val += ft_get_value(base, str[i]);
+            i++;
+        }
+        return (val);
+    }
+    else
+        return (0);
 }
